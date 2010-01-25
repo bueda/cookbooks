e9ad9f500a6574d37a0061cf763a50682555040e
Christopher Peplin <peplin@bueda.com>
2010-01-25 13:00:20 -0500

    Added users and sshd attributes to solo config

---
+++ ssh/recipes/server.rb
@@ -0,0 +1,38 @@
+packages = case node[:platform]
+  when "centos","redhat","fedora"
+    %w{openssh-clients openssh}
+  else
+    %w{openssh-client openssh-server}
+  end
+  
+packages.each do |pkg|
+  package pkg
+end
+
+service "ssh" do
+  case node[:platform]
+  when "centos","redhat","fedora"
+    service_name "sshd"
+  else
+    service_name "ssh"
+  end
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+remote_file "/etc/ssh/known_hosts" do
+  source "known_hosts"
+  mode 0644
+end
+
+template "/etc/ssh/sshd_config" do
+  source "sshd_config.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "ssh")
+end
+
+service "ssh" do
+  action :start
+end
---
+++ users/attributes/default.rb
@@ -0,0 +1,5 @@
+users Mash.new unless attribute?("users")
+groups Mash.new unless attribute?("groups")
+ssh_keys Mash.new unless attribute?("ssh_keys")
+roles Mash.new unless attribute?("roles")
+

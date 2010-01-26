a4b970630db7c44a7b000f1aade8f0cd0c907458
Christopher Peplin <peplin@bueda.com>
2010-01-26 10:52:26 -0500

    Added recipe for denyhosts service and hosts.allow file

--- denyhosts/recipes/default.rb
+++ denyhosts/recipes/default.rb
@@ -2,4 +2,19 @@ package "denyhosts" do
   action :install
 end
 
-#TODO add template for hosts.allow
+service "denyhosts" do
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+remote_file "/etc/hosts.allow" do
+  source "hosts.allow"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "denyhosts")
+end
+
+service "denyhosts" do
+  action :start
+end

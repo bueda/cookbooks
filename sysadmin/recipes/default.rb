5837779b43d352672073f39a7858daa53561b1cb
Christopher Peplin <peplin@bueda.com>
2010-01-26 17:59:57 -0500

    Resolved additional TODOs

--- sysadmin/recipes/default.rb
+++ sysadmin/recipes/default.rb
@@ -5,11 +5,8 @@ package "runurl"
 package "ec2-init"
 
 #TODO media site for nginx?
-#TODO irstlm
 #TODO switch from EBS to ephemeral - grab from dev server with rsync or from S3
-#TODO switch apache, django and mail logs to empemeral storage
 #TODO deploy django 
-#TODO email start/stop scripts
 
 node[:sysadmin][:files].each do |file|
   remote_file "/usr/local/bin/#{file}" do

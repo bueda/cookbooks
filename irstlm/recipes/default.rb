92dae40e71a3a77a590d24c922d305ebdea4bcf9
Christopher Peplin <peplin@bueda.com>
2010-02-02 14:51:24 -0500

    Fixed and tested IRSTLM recipe, #89

--- irstlm/recipes/default.rb
+++ irstlm/recipes/default.rb
@@ -1,6 +1,9 @@
 package "zlib1g-dev"
 
-package "irstlm"
+remote_file "/tmp/irstlm.deb" do
+  source "irstlm_5.30-1_i386.deb"
+end
+
 dpkg_package "irstlm" do
-    source "irstlm_5.30-1_i386.deb"
+  source "/tmp/irstlm.deb"
 end

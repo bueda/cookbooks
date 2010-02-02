2947390e94c40a21580ba1ca69bab2129155e167
Christopher Peplin <peplin@bueda.com>
2010-02-02 16:09:46 -0500

    Switched from ssh_wrapper to just putting key in root's .ssh dir

--- django-deploy/recipes/default.rb
+++ django-deploy/recipes/default.rb
@@ -1,21 +1,26 @@
 require 'chef-deploy'
 
-["deploy_key", "deploy_key.pub"].each do |key|
-  remote_file "/tmp/#{key}" do
-    source key
+directory "/root/.ssh" do
+    owner "root"
+    group "root"
     mode "0600"
-  end
+    not_if "test -d /root/.ssh"
 end
 
-remote_file "/tmp/git-ssh.sh" do
-  remote_file "git-ssh.sh"
+remote_file "/root/.ssh/id_rsa" do
+    source "deploy_key"
+    mode "0600"
+end
+
+remote_file "/root/.ssh/id_rsa.pub" do
+    source "deploy_key.pub"
+    mode "0600"
 end
 
 node[:django_deploy].each do |path, config|
   deploy path do
     repo config[:repo]
     revision config[:revision]
-    git_ssh_wrapper "/tmp/git-ssh.sh"
     user "deploy"
     group "bueda"
     migrate false

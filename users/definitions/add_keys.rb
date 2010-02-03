e99411a051c43faee408ae591ea3f7973b273ba2
Christopher Peplin <peplin@bueda.com>
2010-02-02 19:53:55 -0500

    Added support for multiple ssh keys per user

--- users/definitions/add_keys.rb
+++ users/definitions/add_keys.rb
@@ -1,15 +1,8 @@
 define :add_keys do
-  config = params[:conf]
   name = params[:name]
-  keys = Mash.new
-  keys[name] = node[:ssh_keys][name]
+  config = params[:conf]
+  keys = config[:ssh_keys]
 
-  if config[:extra_ssh_keys]
-    config[:extra_ssh_keys].each do |username|
-      keys[username] = node[:ssh_keys][username]
-    end
-  end
-  
   template "/home/#{name}/.ssh/authorized_keys" do
     source "authorized_keys.erb"
     action :create
@@ -19,4 +12,4 @@ define :add_keys do
     mode 0600
     not_if { defined?(node[:users][name][:preserve_keys]) ? node[:users][name][:preserve_keys] : false }
   end
-end
+end if keys

c83fef7e8fa0c3ccf55f57e03fb8f437bb99269f
Christopher Peplin <peplin@bueda.com>
2010-02-03 17:43:19 -0500

    Added django-media site template

--- sysadmin/recipes/default.rb
+++ sysadmin/recipes/default.rb
@@ -2,9 +2,7 @@ package "vim-gnome"
 package "ack-grep"
 package "htop"
 
-#TODO media site for nginx?
-#TODO switch from EBS to ephemeral - grab from dev server with rsync or from S3
-#TODO deploy django 
+#TODO grab bulk data from S3
 
 node[:sysadmin][:files].each do |file|
   remote_file "/usr/local/bin/#{file}" do
@@ -12,11 +10,3 @@ node[:sysadmin][:files].each do |file|
     mode 0755
   end
 end if node[:sysadmin] and node[:sysadmin][:files]
-
-directory "/var/log/bueda" do
-  owner "www-data"
-  group "bueda"
-  mode "0775"
-  action :create
-  not_if "test -d /var/log/bueda"
-end

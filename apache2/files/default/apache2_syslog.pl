21ead565712cba320b520800bded4a5867ab3a30
Christopher Peplin <peplin@bueda.com>
2010-02-21 21:09:01 -0500

    changed level of syslog script

--- apache2/files/default/apache2_syslog.pl
+++ apache2/files/default/apache2_syslog.pl
@@ -2,9 +2,9 @@
 use Sys::Syslog qw( :DEFAULT setlogsock );
 
 setlogsock('unix');
-openlog('apache', 'cons', 'pid', 'local2');
+openlog('apache', 'cons', 'pid', 'local3');
 
 while ($log = <STDIN>) {
-    syslog('notice', $log);
+    syslog('local3.info', $log);
 }
 closelog

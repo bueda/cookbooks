fc0202924bd53ac06536b7458b6e810058f7c557
Christopher Peplin <peplin@bueda.com>
2010-04-09 12:42:07 -0400

    Simply fab_deploy to actually use Fabric.

--- fab_deploy/attributes/fab_deploy.rb
+++
@@ -1,2 +0,0 @@
-default[:fab_deploy][:releases_directory] = "releases"
-default[:fab_deploy][:packages_directory] = "packages"
--- fab_deploy/recipes/default.rb
+++ fab_deploy/recipes/default.rb
@@ -20,48 +20,23 @@ class Chef::Provider::RemoteFile
   include S3RemoteFile
 end
 
-
 node[:fab_deploy].each do |name, config|
-  if not File.exists?(
-      "/#{config[:deploy_parent]}/releases/#{config[:deploy_target]}") then
+  remote_file "/tmp/#{name}.tar.gz" do
+    source config[:source]
+    owner config[:owner]
+    group config[:group]
+    mode 0755
+  end
 
-    releases_directory = config[:releases_directory] || node[:releases_directory]
-    directory "/#{config[:deploy_parent]}/#{name}/#{releases_directory}" do
-      owner config[:owner]
-      group config[:group]
-      mode 0775
-      recursive true
-    end
+  bash "extract" do
+    user config[:owner]
+    cwd "/tmp"
+    code "tar -xzf /tmp/#{name}.tar.gz"
+  end
 
-    packages_directory = config[:packages_directory] || node[:packages_directory]
-    directory "/#{config[:deploy_parent]}/#{name}/#{packages_directory}" do
-      owner config[:owner]
-      group config[:group]
-      mode 0775
-      recursive true
-    end
-
-    remote_file "/#{config[:deploy_parent]}/#{name}/#{packages_directory}/#{config[:deploy_target])}" do
-      source config[:source]
-      owner config[:owner]
-      group config[:group]
-      mode 0755
-    end
-
-    bash "deploy" do
-      user "root"
-      cwd "/#{config[:deploy_parent]}/#{name}/#{packages_directory}"
-      code <<-EOH
-      tar -xzf #{config[:file]}
-      mv #{config[:extracted_folder]} #{config[:deploy_parent]}/#{releases_directory}/#{config[:deploy_target]}
-      chown #{config[:owner]}:#{config[:group]} -R #{config[:deploy_parent]}/#{releases_directory}/#{config[:deploy_target]}
-      chmod 775 -R #{config[:deploy_parent]}/#{releases_directory}/#{config[:deploy_target]}
-      EOH
-    end
-
-    link "/#{config[:deploy_parent]}/#{releases_directory]}/#{config[:symlink]}" do
-      to "#{config[:deploy_parent]}/#{releases_directory}/#{config[:deploy_target]}"
-      not_if do File.exists?("/#{config[:deploy_parent]}/#{releases_directory]}/#{config[:symlink]}") end
-    end
+  bash "fab" do
+    user config[:owner]
+    cwd "/tmp/#{config[:extracted_folder]}"
+    code "fab localhost deploy"
   end
 end

cc60f01f886c014366eacfc348c481ac7cba7442
Christopher Peplin <peplin@bueda.com>
2010-05-21 12:29:25 -0400

    "Fix" short-file bug by calling file.size before passing to a block.
    There could be a bigger problem, but it also could just be that file.close
    isn't waiting long enough before unlinking.

--- fab_deploy/libraries/s3_remote_file.rb
+++ fab_deploy/libraries/s3_remote_file.rb
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-require 'aws/s3'
+#
+# TODO this code is duplicated in fab_deploy and bulk-s3
 
 class Chef::Resource::RemoteFile
   def access_key_id(args=nil)
@@ -36,32 +36,47 @@ class Chef::Resource::RemoteFile
   end
 end 
 
-
 class Chef::Provider::RemoteFile
-  def get_from_uri(source)
+  def s3_uri?(source)
+    uri = URI.parse(source)
+    uri.absolute? and uri.scheme == "s3"
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def source_file(source, current_checksum, &block)
+    if s3_uri?(source)
+      fetch_from_s3(source, &block)
+    elsif absolute_uri?(source)
+      fetch_from_uri(source, &block)
+    elsif !Chef::Config[:solo]
+      fetch_from_chef_server(source, current_checksum, &block)
+    else
+      fetch_from_local_cookbook(source, &block)
+    end
+  end
+
+  def fetch_from_s3(source)
     begin
       uri = URI.parse(source)
-      if uri.absolute
-        if uri.scheme == "s3"
-          AWS::S3::Base.establish_connection!(
-              :access_key_id     => @new_resource.access_key_id,
-              :secret_access_key => @new_resource.secret_access_key
-          )
-          name = uri.path[1..-1]
-          bucket = uri.host
-          obj = AWS::S3::S3Object.find name, bucket
-          Chef::Log.debug("Downloading #{name} from S3 bucket #{bucket}")
-          file = Tempfile.new("chef-s3-file")
-          file.write obj.value
-          file.close
-          file
-        else
-          r = Chef::REST.new(source, nil, nil)
-          Chef::Log.debug("Downloading from absolute URI: #{source}")
-          r.get_rest(source, true).open
-        end
+      AWS::S3::Base.establish_connection!(
+          :access_key_id     => @new_resource.access_key_id,
+          :secret_access_key => @new_resource.secret_access_key
+      )
+      name = uri.path[1..-1]
+      bucket = uri.host
+      obj = AWS::S3::S3Object.find name, bucket
+      Chef::Log.debug("Downloading #{name} from S3 bucket #{bucket}")
+      file = Tempfile.new("chef-s3-file")
+      file.write obj.value
+      Chef::Log.debug("File #{name} is #{file.size} bytes on disk")
+      begin
+        yield file
+      ensure
+        file.close
       end
     rescue URI::InvalidURIError
+      Chef::Log.warn("Expected an S3 URL but found #{source}")
       nil
     end
   end

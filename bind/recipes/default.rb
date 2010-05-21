17056acf467664c98849fa430504bf11ef00b4a2
Christopher Peplin <peplin@bueda.com>
2010-05-21 18:20:44 -0400

    Make bind recipe idempotent.

--- bind/recipes/default.rb
+++ bind/recipes/default.rb
@@ -1,14 +1,26 @@
 node[:bind].each do |path, config|
-  [path, config[:dev]].each do |dir|
-    directory dir do
-      owner config[:owner]
-      group config[:group]
-      mode "0775"
-      recursive true
-      action :create
-      not_if "test -d #{dir}"
-    end 
+  if not File.exists?(config[:dev])
+    if File.exists?(path)
+      FileUtils.mv path, config[:dev]
+    else
+      directory config[:dev] do
+        owner config[:owner]
+        group config[:group]
+        mode "0775"
+        recursive true
+        action :create
+      end 
+    end
   end
+
+  directory path do
+    owner config[:owner]
+    group config[:group]
+    mode "0775"
+    recursive true
+    action :create
+  end
+
   mount path do
     device config[:dev]
     fstype "none"

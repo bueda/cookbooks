b40fe1aaf4ee230dca6f1d0469d63a25d382f1bd
Christopher Peplin <peplin@bueda.com>
2010-08-26 12:42:04 -0400

    add celeryd cookbook.

---
+++ celeryd/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: celery
+# Recipe:: default
+#
+# Copyright 2010, Bueda Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+require_recipe "god"
+
+search(:repos, "*:*") do |app|
+    god_monitor app.id do
+        config "celeryd.god.erb"
+    end
+end
